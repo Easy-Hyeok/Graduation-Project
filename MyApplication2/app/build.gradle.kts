eba27e691817de13cff29453b63b62e000e0a6ab
Jihyeok Lee <dlwlgurw@gamil.com>
2024-02-13 17:30:32 +0900

    fp-jihyeok

--- MyApplication2/app/build.gradle.kts
+++ MyApplication2/app/build.gradle.kts
@@ -47,9 +47,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     //noinspection GradleCompatible
     implementation("com.android.support:design:28.0.0")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation ("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("com.google.firebase:firebase-firestore:24.10.2")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
     implementation("de.hdodenhof:circleimageview:3.1.0")
@@ -57,4 +57,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
